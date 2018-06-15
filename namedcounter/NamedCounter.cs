313f570f0969575a8e1be16c438bb79cea7fa8ce
Neil Swinton <neil@pineknollconsulting.com>
2018-06-14 22:14:28 -0400

    Initial implementation

--- namedcounter/NamedCounter.cs
+++ namedcounter/NamedCounter.cs
@@ -1,11 +1,31 @@
-using System.Collections.Generic;
-using System.IO;
+/**
+Copyright(c) 2018 Pine Knoll Consulting, LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+**/
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Newtonsoft.Json;
+
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Configuration;
@@ -17,12 +37,10 @@ namespace namedcounter
 {
     public static class NamedCounter
     {
-        //# static private readonly string storageAccount = "devstoreaccount1";
-        // # static private readonly string storageAccountKey = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
-
-        
         public class CounterEntity : TableEntity
         {
+            public static readonly string tableName = "namedCounters";
+
             public CounterEntity(string userId, string name, System.Int64 counter)
             {
                 this.PartitionKey = userId;
@@ -48,39 +66,39 @@ namespace namedcounter
 
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
             CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-            CloudTable table = tableClient.GetTableReference("namedCounters");
+            CloudTable table = tableClient.GetTableReference(CounterEntity.tableName);
 
             return table;
         }
 
         [FunctionName("Next")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log, ExecutionContext context)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "Next/{nameParameter}")]HttpRequest req, 
+                                                    string nameParameter, TraceWriter log, ExecutionContext context)
         {
-            log.Info(string.Format("C# HTTP trigger function {0} processed a request.", req.Method));
-
-            string nameParameter = req.Query["name"];
+            log.Verbose("C# HTTP trigger function processed a request.");
             string countParameter = req.Query["count"];
             string resetParameter = req.Query["reset"];
             string userId = req.Query["code"];
 
-            if (req.Headers.ContainsKey("x-functions-key"))
-                userId = req.Headers["x-functions-key"].ToString();
-
+            // User ID from code query param of x-functions-key header
             if (userId == null)
-                 userId = "Joe User";
+                userId = req.Headers["x-functions-key"].ToString();
+            if (userId == string.Empty)
+                userId = "Development";
 
-
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            nameParameter = nameParameter ?? data?.name;
-            countParameter = countParameter ?? data?.count ?? "1";
-            resetParameter = resetParameter ?? data?.reset ?? "False";
-
-            if (countParameter == string.Empty)
+            // Default count to an increment of one
+            if (countParameter == null)
                 countParameter = "1";
 
-            if (resetParameter == string.Empty)
-                resetParameter = true.ToString();
+            // Allow both ?reset=True and ?reset to reset counter
+            if (resetParameter == null)
+                resetParameter = bool.FalseString;
+            else if (resetParameter == string.Empty)
+                resetParameter = bool.TrueString;
+
+            // Bail early on bad inputs
+            if (nameParameter == null)
+                return new BadRequestObjectResult("The name parameter must be present");
 
             if (!System.Int64.TryParse(countParameter, out System.Int64 counterIncrement) || counterIncrement < 0)
                 return new BadRequestObjectResult($"count value must be a positive integer: \"{countParameter}\"");
@@ -88,51 +106,53 @@ namespace namedcounter
             if (!bool.TryParse(resetParameter, out bool counterReset))
                 return new BadRequestObjectResult($"reset value must be true/false: \"{resetParameter}\"");
 
-            // TODO validate and set reset_counter and counter_increment
+            // Normalize inputs
+            nameParameter = nameParameter.ToLower();
+            userId = userId.ToLower();
 
-            if (nameParameter != null)
+
+            // Get a reference to the table and create it as needed
+            CloudTable table = getTableClient(context);
+            if (await table.CreateIfNotExistsAsync())
+                log.Info($"Created table {CounterEntity.tableName}");
+
+            // Read the current counter record if it exists
+            TableOperation tableOperation = TableOperation.Retrieve(userId, nameParameter);
+            TableResult retrievedResult = await table.ExecuteAsync(tableOperation);
+
+            // TODO: Loop on concurrancy errors
+            CounterEntity counterEntity;
+            string operationMessage;
+
+            if (retrievedResult.Result == null)
             {
-                CloudTable table = getTableClient(context);
-                bool created = await table.CreateIfNotExistsAsync();
-                log.Info(string.Format("C# HTTP trigger function table created: {0}.", created));
-
-                // Create a retrieve operation that takes a customer entity.
-                TableOperation tableOperation = TableOperation.Retrieve(userId, nameParameter);
-                // Execute the retrieve operation.
-                TableResult retrievedResult = await table.ExecuteAsync(tableOperation);
-
-
-                // TODO: Loop on concurrancy errors
-                CounterEntity counterEntity;
-
-
-                if (retrievedResult.Result == null)
-                {
-                    // First time -- do an insert.  Fails on a concurrency error
-                    log.Verbose(string.Format("No record found for key {0}", nameParameter));
-                    counterEntity = new CounterEntity(userId, nameParameter, counterIncrement);
-                    tableOperation = TableOperation.Insert(counterEntity, true);
-                }
-                else
-                {
-                    log.Verbose(string.Format("Record found for key {0}", nameParameter));
-                    DynamicTableEntity namedCounter = (DynamicTableEntity)retrievedResult.Result;
-                    namedCounter.Properties.TryGetValue("CurrentCount", out EntityProperty counterProperty);
-                    System.Int64 updatedCount = counterReset ? 0 : (System.Int64) counterProperty.Int64Value;
-                    updatedCount += counterIncrement;
-                    counterEntity = new CounterEntity(userId, nameParameter, updatedCount)
-                    {
-                        ETag = retrievedResult.Etag
-                    };
-                    tableOperation = TableOperation.InsertOrReplace(counterEntity);
-                }
-                TableResult tableResult = await table.ExecuteAsync(tableOperation);
-                log.Info("TableResult");
-                return (ActionResult)new OkObjectResult($"{counterEntity.CurrentCount}");
+                // First time -- do an insert.  If another process inserts first, this fails (appropriately)
+                operationMessage = $"Creating new entry for {userId}:{nameParameter} = {counterIncrement}";
+                counterEntity = new CounterEntity(userId, nameParameter, counterIncrement);
+                tableOperation = TableOperation.Insert(counterEntity, true);
             }
-            return nameParameter != null
-                ? (ActionResult)new OkObjectResult($"Hello, {nameParameter}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            else
+            {
+                // Update the current value
+                DynamicTableEntity namedCounter = (DynamicTableEntity)retrievedResult.Result;
+                namedCounter.Properties.TryGetValue("CurrentCount", out EntityProperty counterProperty);
+                System.Int64 updatedCount = counterReset ? 0 : (System.Int64)counterProperty.Int64Value;
+                updatedCount += counterIncrement;
+                operationMessage = $"Updating {userId}:{nameParameter} = {counterIncrement}";
+                counterEntity = new CounterEntity(userId, nameParameter, updatedCount)
+                {
+                    ETag = retrievedResult.Etag
+                };
+                tableOperation = TableOperation.Replace(counterEntity);
+            }
+
+            // Execute the table operation -- Insert or Replace
+            TableResult tableResult = await table.ExecuteAsync(tableOperation);
+            log.Info($"Status: {tableResult.HttpStatusCode}.  {operationMessage}");
+            if (tableResult.HttpStatusCode < 300)
+                return (ActionResult)new OkObjectResult($"{counterEntity.CurrentCount}");
+            else
+                return (ActionResult)new StatusCodeResult(tableResult.HttpStatusCode);
         }
     }
 }
